0e090db651be58f429a2908356c31c002b18ac19
Tatakinov <tatakinov@gmail.com>
2023-08-06 18:23:22 +0900

    必要の無い計算を省くようにした。substrの高速化。

--- levenshtein.lua
+++ levenshtein.lua
@@ -14,49 +14,47 @@ local function id(s1, s2)
 end
 
 local _cache  = {}
+local _d_max  = 10000
 
-local function init_cache(s1, s2)
-  _cache  = {}
+local function init_cache(d_max, s1, s2)
+  _d_max  = d_max
   _cache["/"]  = 0
   for i = 1, #s1 do
-    _cache[id(string.sub(s1, 1, i), "")] = i
+    _cache[id(string.sub(s1, i), "")] = #s1 - i + 1
   end
   for i = 1, #s2 do
-    _cache[id("", string.sub(s2, 1, i))] = i
+    _cache[id("", string.sub(s2, i))] = #s2 - i + 1
   end
 end
 
 local function cache(s1, s2)
-  if _cache[id(s1, s2)] then
-    return _cache[id(s1, s2)]
+  local i  = id(s1, s2)
+  if _cache[i] then
+    --print("cache:", s1, s2)
+    return _cache[i]
   end
-  local cost = string.sub(s1, -1, -1) == string.sub(s2, -1, -1) and 0 or 1
-  _cache[id(s1, s2)] = min(
-    cache(string.sub(s1, 1, -2), s2) + 1,
-    cache(s1, string.sub(s2, 1, -2)) + 1,
-    cache(string.sub(s1, 1, -2), string.sub(s2, 1, -2)) + cost
+  if math.abs(#s1 - #s2) > _d_max then
+    _cache[i]  = _d_max + 1
+    return _d_max + 1
+  end
+  local cost = string.sub(s1, 1, 1) == string.sub(s2, 1, 1) and 0 or 1
+  _cache[i] = min(
+    cache(string.sub(s1, 2), s2) + 1,
+    cache(s1, string.sub(s2, 2)) + 1,
+    cache(string.sub(s1, 2), string.sub(s2, 2)) + cost
   )
   return _cache[id(s1, s2)]
 end
 
-local function getLevenshteinDistance(s1, s2)
-  init_cache(s1, s2)
-  return cache(s1, s2)
-end
-
-local function M(s, ...)
-  local t = {...}
-  local key
-  for _, v in ipairs(t) do
-    for k, v in pairs(v) do
-      if s ~= k then
-        if getLevenshteinDistance(s, k) <= 2 then
-          key = k
-        end
-      end
-    end
+local function M(d_max, s1, s2)
+  if s1 == s2 then
+    return 0
   end
-  return key
+  if s1 > s2 then
+    s1, s2  = s2, s1
+  end
+  init_cache(d_max, s1, s2)
+  return cache(s1, s2)
 end
 
 return M
